95f48695c5513b7e01d98389f5af6b5a6c8b912d
cyc <xsj@example.com>
2026-09-23 11:22:42 +0800

    feature: Guessing the number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess:u32 = match guess.trim().parse(){
+            Ok(num)=>{num},
+            Err(_)=>{println!("Please input vaild number"); continue;},
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less=>println!("Too less"),
+            Ordering::Greater=>println!("Too greater"),
+            Ordering::Equal=>{println!("You win, congratulations!"); break;},
+        };
+    };
+
 }
